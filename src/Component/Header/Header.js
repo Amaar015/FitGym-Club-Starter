244d6794d38b0d55a221d0ce7bef0a303403b91a
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2022-12-19 23:01:56 +0500

    navbar responsive

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css';
 import logo from './../../assets/logo.png';
 const Header = () => {
-  return (
+       const mobile=window.innerWidth<=786? true:false;
+       const [menuOpened,SetmenuOpened]=useState(false);
+     return (
    <div className="header">
             <img src={logo} alt=""  className='logo'/>
+              {/* {(menuOpened===false && menuOpened===true)?{
+                <div>
+
+                </div>
+              }} */}
             <ul className='header-menu'>
                 <li>Home</li>
                 <li>Programs</li>
